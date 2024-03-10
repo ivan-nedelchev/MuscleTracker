ac574a4e0b0f12cdc5345f478481b5d791eda242
Ivan Nedelchev <130435484+ivan-nedelchev@users.noreply.github.com>
2024-03-10 23:03:16 +0200

    Added Navbar + React Router

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import "./Navbar.css"
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav>
+      <Link to="/" className='title'>
+        MuscleTracker
+      </Link>
+      <div 
+        className='menu'
+        onClick={ () => {
+          setMenuOpen(!menuOpen)
+        }}
+        >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul class={ menuOpen ? 'open' : ''}>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/register">Register</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/views/index.js
@@ -0,0 +1,12 @@
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Register from './Register'
+
+
+export {
+    Home,
+    About,
+    Register,
+    Login
+}
